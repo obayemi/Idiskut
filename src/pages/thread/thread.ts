e39eff605f26b17400211de52fb8fb4fc99683c0
Maxime GUITTET <maxime.guittet@epitech.eu>
2018-01-27 18:00:34 +0100

    update input message

--- src/pages/thread/thread.ts
+++ src/pages/thread/thread.ts
@@ -21,6 +21,8 @@ export class ThreadPage {
     private messagesRef: any;
     private user: any;
 
+    public message: any;
+
     constructor(
         public navCtrl: NavController,
         public navParams: NavParams,
@@ -48,9 +50,8 @@ export class ThreadPage {
         return this.thread.data().name;
     }
 
-    ionViewDidLoad() {
-        //console.log('ionViewDidLoad ThreadPage');
-        this.postMessage('test')
+    onSubmit() {
+        this.postMessage(this.message)
+        this.message = ""
     }
-
 }
